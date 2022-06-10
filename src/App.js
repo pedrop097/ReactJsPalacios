6a4c09a5233e380a31005f4362939eeaf3f9242d
Pedro Palacios <pedro097@hotmail.com>
2022-06-09 23:26:50 -0300

    Primer entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <h1>DIONISO SHOP DE BEBIDAS</h1>
+        <p>Somos una empresa dedicada a la venta de bebidas alcoholicas, especialmente de vinos.</p>
+        <img src="./logotico.png"></img>
+              </header>
     </div>
   );
 }
