143565620641d6ca92b4871240b3b2c7931f50a5
Pedro Palacios <pedro097@hotmail.com>
2022-06-11 00:15:31 -0300

    Agregue NavBar a mi proyecto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 // import logo from './logo.svg';
+//import React from 'react';
+
 import './App.css';
+import Nav from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
-         <h1>DIONISO SHOP DE BEBIDAS</h1>
+
+        <h1>DIONISO SHOP DE BEBIDAS</h1>
         <p>Somos una empresa dedicada a la venta de bebidas alcoholicas, especialmente de vinos.</p>
         <img src="./logotico.png"></img>
-              </header>
+
+
+      </header>
+
     </div>
   );
 }
