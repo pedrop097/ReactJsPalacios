03254ea9749bd2e9526bc42d66ad78f4ad464e24
Pedro Palacios <pedro097@hotmail.com>
2022-06-20 12:27:17 -0300

    Agregue boton sumar y restar a mi proyecto

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@
 //import React from 'react';
 //@ts-check
 
-import { logDOM } from '@testing-library/react';
 import './App.css';
 import Nav from './Components/NavBar';
 import ItemListContainer from './Components/ItemListContainer';
 import ItemCount from './Components/ItemCount';
-import { useState } from 'react';
 
 
-let miProd = {id:0, img:"./corona.jpeg", name:"vino malbec", price: 100};
 
 let initial = 1;
 let stock = 5;
 
- 
+
 function App() {
   return (
     <div className="App">
@@ -25,25 +22,21 @@ function App() {
         <ItemListContainer saludo="BIENVENIDO A DIONISO SHOP DE BEBIDAS" />
         <p>Somos una empresa dedicada a la venta de bebidas alcoholicas, especialmente de vinos.</p>
         <img src="./logotico.png"></img>
-       
-        <div className="card" style={{width: "18rem"}}>
-  <img src="./almamora.png" className="card-img-top" alt="..."/>
-  
-</div>
-  
+
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="./almamora.png" className="card-img-top" alt="..." />
+        </div>
+
         <div>
 
-
-
-            
-    <ItemCount  miProd={miProd}
-   stock={stock}
-   initial={initial}
-  
-    />
-   </div>
+          <ItemCount
+            stock={stock}
+            initial={initial}
+          
+          />
+        </div>
       </header>
-  
+
     </div>
   );
 }
--- src/Components/ItemCount.js
+++ src/Components/ItemCount.js
@@ -1,38 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 
-export default function ItemCount({stock, initial}) {
- //let carrito = initial;
+export default function ItemCount({ stock, initial}) {
+
+
+    const [carrito, setCarrito] = useState(initial);
+
  
- const[carrito, setCarrito] = useState(initial);
 
-
-    //  useEffect(() =>{
-    //     if (carrito <= 5){
-    //         initial++;
-    //          }
-
-    //  }, []);
-  
-    function aumentarStock  (){
-        if(carrito <5 ){
-setCarrito(carrito + 1);
+   
+    function restar   (){
+        setCarrito(carrito - 1);
+        if (carrito > 0)
+        setCarrito(carrito - 1)
+        else{
+            setCarrito(carrito);
         }
-else{
-setCarrito(carrito);
-}
-
-  }
+       }
+       function onAdd (){
+        alert("Productos en Carrito:"+ " " + (carrito));
+       }
+    function sumar() {
+        if (carrito < 5) {
+            setCarrito(carrito + 1);
+        }
+        else {
+            setCarrito(carrito);
+        }
+ 
+          }
     return (
-    <div>
-      <p>Vino Alma Mora</p>
-      <p>$1000</p>
-       <p>Productos Añadidos : {carrito}</p>
-      <p> Maximo Stock Permitido : {stock} </p>
-      
-      <p><button onClick={() => aumentarStock()}
-       style={{border: "3px solid black"}}>Agregar Item</button></p>
-
+        <div>
+            <p>Vino Alma Mora</p>
+            <p>$1000</p>
+            <p>Productos Añadidos : {carrito}</p>
+            <p> Maximo Stock Permitido : {stock} </p>
+<div className='sumarRestar'>
+            <div>
+        <p><button onClick={() => sumar()}  style={{border: "2px solid black"}} >+</button></p>
     </div>
-  )
+    
+    <div>
+        <p><button onClick={() =>  restar()}  style={{ border: "2px solid black", width: "29px" }}>-</button></p>
+    </div>
+    </div>
+            <p><button  onClick={() => onAdd()}
+                style={{ border: "3px solid black" }}>Agregar al Carrito</button></p>
+
+        </div>
+    )
 }
+
