2ccdcc72826a782b4a249adae60098d9cf0ef11b
Pedro Palacios <pedro097@hotmail.com>
2022-06-20 12:32:17 -0300

    modifique alert

--- src/Components/ItemCount.js
+++ src/Components/ItemCount.js
@@ -1,25 +1,28 @@
 import React, { useState } from 'react'
 
 
-export default function ItemCount({ stock, initial}) {
+export default function ItemCount({ stock, initial }) {
 
 
     const [carrito, setCarrito] = useState(initial);
 
- 
 
-   
-    function restar   (){
+    function restar() {
         setCarrito(carrito - 1);
         if (carrito > 0)
-        setCarrito(carrito - 1)
-        else{
+            setCarrito(carrito - 1)
+        else {
             setCarrito(carrito);
         }
-       }
-       function onAdd (){
-        alert("Productos en Carrito:"+ " " + (carrito));
-       }
+    }
+    function onAdd() {
+        if (carrito > 0) {
+            alert("Productos en Carrito:" + " " + (carrito));
+        }
+        else {
+            setCarrito(carrito);
+        }
+    }
     function sumar() {
         if (carrito < 5) {
             setCarrito(carrito + 1);
@@ -27,24 +30,24 @@ export default function ItemCount({ stock, initial}) {
         else {
             setCarrito(carrito);
         }
- 
-          }
+
+    }
     return (
         <div>
             <p>Vino Alma Mora</p>
             <p>$1000</p>
             <p>Productos Añadidos : {carrito}</p>
             <p> Maximo Stock Permitido : {stock} </p>
-<div className='sumarRestar'>
-            <div>
-        <p><button onClick={() => sumar()}  style={{border: "2px solid black"}} >+</button></p>
-    </div>
-    
-    <div>
-        <p><button onClick={() =>  restar()}  style={{ border: "2px solid black", width: "29px" }}>-</button></p>
-    </div>
-    </div>
-            <p><button  onClick={() => onAdd()}
+            <div className='sumarRestar'>
+                <div>
+                    <p><button onClick={() => sumar()} style={{ border: "2px solid black" }} >+</button></p>
+                </div>
+
+                <div>
+                    <p><button onClick={() => restar()} style={{ border: "2px solid black", width: "29px" }}>-</button></p>
+                </div>
+            </div>
+            <p><button onClick={() => onAdd()}
                 style={{ border: "3px solid black" }}>Agregar al Carrito</button></p>
 
         </div>
