94807cfc803b079aa2a82bd75c2329336ad2ab0b
Pedro Palacios <pedro097@hotmail.com>
2022-07-11 23:08:12 -0300

    Modificando item detail

--- src/Components/ItemCount.jsx
+++ src/Components/ItemCount.jsx
@@ -1,51 +1,31 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
 
 
-export default function ItemCount({ stock, initial, onAdd}) {
+export default function ItemCount({ stock, initial}) {
 
-  const[changeButton, setChangeButton] = useState(true)  
-const [cant,setCant] = useState(initial)
-const Add =  ()=> {
-    setCant(cant + 1)
-}
-const Res = () => {
-    setCant(cant - 1)
-}
-return(
+    const [cant, setCant] = useState(initial)
 
-    
-<>
+   
+    const Add = () => {
+        setCant(cant + 1)
+    }
+    const Res = () => {
+        setCant(cant - 1)
+    }
+    return (
+        <>
+            <div >
 
-    <div >
-              <button  onClick={ ()=> setChangeButton(!changeButton)} >
-                  Agregar al carrito
-                </button>
-                {changeButton ? (
-                  
-                      <div style={{margin:"10px", display:"flex", justifyContent:"space-evenly"}}>
-                      <button style={{margin:"10px", height:"32px"}} onClick={Add} disabled={cant === stock}>+</button>
-                      <h1>{cant}</h1>
-                      <button style={{margin:"10px", height:"32px"}} onClick={Res} disabled={cant === initial}>-</button>
-                      </div>
-                 
-                               
-                ) : (
-                    <div>
-                        <p>{onAdd(cant)}</p>
-                        <button> 
-              <Link to={'/cart'} style={{ color: 'inherit', textDecoration: 'none' }} >  Finalizar mi compra </Link> 
-            </button> 
-                        
-                        </div>
-                )
-                
-                }
-              </div>
-
-    
-    </>
-    
+                <div style={{ margin: "10px", display: "flex", justifyContent: "space-evenly" }}>
+                    
+                    
+                    <button style={{ margin: "10px", height: "32px" }} onClick={Res} disabled={cant === initial}>-</button>
+                     <h1>{cant}</h1>
+                    <button style={{ margin: "10px", height: "32px" }} onClick={Add} disabled={cant === stock}>+</button>
+                    
+                </div>
+            </div>
+        </>
     )
 
 }
--- src/Components/ItemDetailContainer/ItemDetail.jsx
+++ src/Components/ItemDetailContainer/ItemDetail.jsx
@@ -1,32 +1,59 @@
 import React from 'react'
 import ItemCount from '../ItemCount'
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
 
 
-export default function ItemDetail({title, price, img, descripcion}) {
+export default function ItemDetail({ title, price, img, descripcion, cant}) {
+
+  
+  const [changeButton, setChangeButton] = useState(true);
   function onAdd(cant) {
     alert(`Agregue ${cant} ${title}  al carrito`);
+ 
   }
+
   return (
-    <div style={{border:"2px solid black", margin:"30px"}}>
-  <div style={{width: "18rem", color:"black", margin:"20px", display:"flex",alignItems:"center"}}>
-    <img src={img} className="card-img-top" style={{width:"700px"}} alt="Dioniso"/>
-    <div className="card-body">
-      <div style={{margin:"10px"}}>
-      <h3  style={{color: "darkgrey"}} >{title}</h3>
-      <h5>Descripcion: {descripcion}</h5>
-      <h4>Precio ${price}</h4>
-      </div>
-
-   <ItemCount
+    <div style={{ border: "2px solid black", margin: "30px" }}>
+      <div style={{ width: "18rem", color: "black", margin: "20px", display: "flex", alignItems: "center" }}>
+        <img src={img} className="card-img-top" style={{ width: "700px" }} alt="Dioniso" />
+        <div className="card-body">
+          <div style={{ margin: "10px" }}>
+            <h3 style={{ color: "darkgrey" }} >{title}</h3>
+            <h5>Descripcion: {descripcion}</h5>
+            <h4>Precio ${price}</h4>
+          </div>
+          <button onClick={() =>  setChangeButton(!changeButton)}>
+            Agregar Productos
+           </button>
+          {changeButton ? (
+          <>
+ <ItemCount
+            
             stock={5}
-            initial={1}                 
-          onAdd={onAdd}
-          />
-         
+            initial={1}
+          
+          /> 
 
+         </>
+           
 
-    </div>
-  </div>
+          ) : (
+
+            <div>
+            <button  onAdd={onAdd(cant)}>
+
+<Link to={'/cart'} style={{ color: 'inherit', textDecoration: 'none' }}>  Finalizar mi compra </Link>
+
+</button>
+
+              
+
+            </div>
+          )}
+
+        </div>
+      </div>
     </div>
   )
 }
